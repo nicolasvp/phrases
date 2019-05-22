c4a091034f8e7d7dea7acb3c9c5ffbb2c1dc44a2
nicolas <nicolas@wedev.cl>
2019-05-21 23:18:44 -0400

    Added bootstrap.yml for config server

---
+++ src/main/resources/import.sql
@@ -0,0 +1,5 @@
+INSERT INTO types (name, created_at) VALUES('Inspiradora', NOW());
+INSERT INTO authors (name, created_at) VALUES('Socrates', NOW());
+INSERT INTO phrases (body, author_id, type_id, created_at) VALUES('El conocimiento empieza en el asombro', '1', '1', NOW());
+INSERT INTO phrases (body, author_id, type_id, created_at) VALUES('El amigo debe ser como el dinero; antes de necesitarlo, es necesario saber su valor', '1', '1', NOW());
+
