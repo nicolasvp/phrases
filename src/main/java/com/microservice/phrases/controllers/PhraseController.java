b45d5e4650a1f3222a5c372f5f0a61721b95e52e
nicolas <nicolas@wedev.cl>
2019-05-23 00:01:02 -0400

    Added feign and hystrix

--- src/main/java/com/microservice/phrases/controllers/PhraseController.java
+++ src/main/java/com/microservice/phrases/controllers/PhraseController.java
@@ -1,24 +1,51 @@
 package com.microservice.phrases.controllers;
 
 import java.util.List;
+import java.util.logging.Logger;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import com.microservice.phrases.models.entity.Phrase;
 import com.microservice.phrases.models.services.IPhraseService;
+import com.microservice.phrases.models.services.remote.IUserRemoteCallService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
 @RestController
 @RequestMapping("/api")
 public class PhraseController {
 
+	protected Logger LOGGER = Logger.getLogger(PhraseController.class.getName());
+	
 	@Autowired
 	private IPhraseService phraseService;
+		
+	@Autowired
+	private IUserRemoteCallService loadBalancer;
 	
 	@GetMapping("/phrases")
 	public List<Phrase> index(){
 		return phraseService.findAll();
+	}
+	
+	@GetMapping("/service-route")
+	public String serviceRoute() {
+		return "Hi from phrases service";
+	}
+	
+	@HystrixCommand(fallbackMethod = "unavailableMessage")
+	@GetMapping("/phrases/users")
+	public String users(){
+		LOGGER.info("Invoking users service from phrase service");
+		String response = loadBalancer.getServiceRoute();
+		return response;
+	}
+		
+	public String unavailableMessage() {
+		return "Users service is not available";
 	}
 }
---
+++ src/main/java/com/microservice/phrases/models/services/remote/IUserRemoteCallService.java
@@ -0,0 +1,11 @@
+package com.microservice.phrases.models.services.remote;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@FeignClient(name="USERS-SERVICE") // Service name registered on Eureka Server
+public interface IUserRemoteCallService {
+	@RequestMapping(method=RequestMethod.GET, value="/api/service-route")
+	public String getServiceRoute();
+}
