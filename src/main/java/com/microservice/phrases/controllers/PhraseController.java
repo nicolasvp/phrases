daec6a04e9b03d05be315df42ec61f623f8c104b
nicolasvp <nicolasverapalominos@gmail.com>
2019-07-05 13:54:15 -0500

    Added CORS to Controllers

--- src/main/java/com/microservice/phrases/controllers/PhraseController.java
+++ src/main/java/com/microservice/phrases/controllers/PhraseController.java
@@ -16,6 +16,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -28,6 +29,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.microservice.phrases.models.entity.Phrase;
 import com.microservice.phrases.models.services.IPhraseService;
 
+@CrossOrigin(origins = { "http://localhost:4200" })
 @RestController
 @RequestMapping("/api")
 public class PhraseController {
